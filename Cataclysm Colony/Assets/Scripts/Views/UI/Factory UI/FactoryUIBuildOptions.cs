8777585460e9d723a6522c732423cbe5ce03a331
mkirkpatrick <mtraviskirkpatrick@gmail.com>
2017-08-26 16:48:29 -0500

    Factory Upgrade Buttons added
    more factory work. Also Factory object and plot both activate UI now.

---
+++ Cataclysm Colony/Assets/Scripts/Views/UI/Factory UI/FactoryUIBuildOptions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FactoryUIBuildOptions : MonoBehaviour {
+
+    public Button colonistIncreaseButton;
+    public Button colonistDecreaseButton;
+    public Button itemIncreaseButton;
+    public Button itemDecreaseButton;
+
+    public Transform upgradesContainer;
+}
---
+++ Cataclysm Colony/Assets/Scripts/Views/UI/Factory UI/FactoryUpgradeButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FactoryUpgradeButton : MonoBehaviour {
+
+    public ItemUpgrade buttonUpgrade;
+
+    public Image upgradeIcon;
+
+    public bool isSelected = false;
+
+    public void Init(ItemUpgrade upgrade)
+    {
+        buttonUpgrade = upgrade;
+        upgradeIcon.sprite = buttonUpgrade.icon;
+    }
+
+    public void UpgradeButtonAction()
+    {
+        UIController.Instance.factoryUI.selectedTask.buildItem.currentUpgrade = buttonUpgrade;
+        UIController.Instance.factoryUI.UpdateDescription();
+    }
+}
